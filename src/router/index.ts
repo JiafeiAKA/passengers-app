f26717b9413f25eb69179d324f3b36ecb0352037
JiafeiAKA <153718860+JiafeiAKA@users.noreply.github.com>
2024-08-08 21:20:34 +0700

    6.3

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,7 @@ import PassengerEditView from '@/views/event/PassengerEditView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
 import NetworkErrorView from '@/views/NetworkErrorView.vue'
 import PassengerListView from '@/views/PassengerListView.vue'
+import nProgress from 'nprogress'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -61,6 +62,14 @@ const router = createRouter({
       component: NetworkErrorView
     }
   ]
+})
+
+router.beforeEach(() => {
+  nProgress.start()
+})
+
+router.afterEach(() => {
+  nProgress.done()
 })
 
 export default router
